cb727aebc55c049432876502fddb29c85c2bf982
solitdio079 <solitdio079@gmail.com>
2025-12-28 14:37:08 +0300

    Learn states

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-const poem = {
-  lines: [
-    'I write, erase, rewrite',
-    'Erase again, and then',
-    'A poppy blooms.'
-  ]
-};
+import { useState } from "react";
 
-export default function Poem() {
+export default function Person() {
+  const [person, setPerson] = useState({ name: "John", age: 100 });
+
+  const handleIncreaseAge = () => {
+    console.log("in handleIncreaseAge (before setPerson call): ", person);
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+    setPerson((prevPerson) => ({ ...prevPerson, age: prevPerson.age + 1 }));
+    // we've called setPerson, surely person has updated?
+    console.log("in handleIncreaseAge (after setPerson call): ", person);
+  };
+
+  // this console.log runs every time the component renders
+  // what do you think this will print?
+  console.log("during render: ", person);
+
   return (
-    <article>
-      {poem.lines.map((line, index) =><>
-        <p key={index}>
-          {line}
-        </p>
-        {index!==poem.lines.length-1 ? <hr key={index + '-separator'}/>:null}
-        </>
-      )}
-    </article>
+    <>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+    </>
   );
 }
